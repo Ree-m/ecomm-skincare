5333e78d7ef0cb6416861b3c9f9ec2a4cabf280e
Reem <reembsrat@gmail.com>
2022-11-15 21:49:39 +0300

    updated config file

--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth");
 const mainController = require("../controllers/main");
+const userController=require("..c")
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Main Routes - simplified for now
